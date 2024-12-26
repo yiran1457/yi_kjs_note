c7de8c34a47354e80849d244e6865398bc4482be
yi_ran <164875652+yiran1457@users.noreply.github.com>
2024-12-27 00:46:45 +0800

    Delete Man! directory

--- Man!/What Can I Say.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace yi {
-    namespace tetra {
-        type key0 = "key"|"primary"|"secondary"|"tertiary"|"durability"|"integrityCost"|"integrityGain"|"magicCapacity"|"toolLevel"|"toolEfficiency"
-        type key1 = "tints"|"textures"|"material"
-        type key2 = "glyph"|"texture"|"items"
-        type requiredTools = "axe_dig"
-        type MaterialType = "wood"|"stone"|"metal"|"gem"
-    }
-}
